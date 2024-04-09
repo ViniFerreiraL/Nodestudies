8b495e216ab1ba55aba469796955be166749858f
Vinicius Ferreira <ferreiravinilugo@gmail.com>
2024-04-09 17:03:17 -0300

    Finish

--- Index.js
+++ Index.js
@@ -3,26 +3,59 @@
 1- Obter numero de telefone do usuario a partir do id
 2- Obter o endereço do usuário
 */
-function Obterusuario() {
+function ObterUsuario(callback) {
   setTimeout(function () {
-    return {
+    return callback(null, {
       id: 1,
-      nome: "Vinicius",
-      datadeaniversario: new Date(),
-    };
-  }, 1000);
-}
-function Obtertelefone() {
-  setTimeout(function () {
-    return {
-      telefone: "999322427",
-      ddd: 21,
-    };
+      Nome: "Vinicius",
+      DatadeNascimento: new Date(),
+    });
   });
+  1000;
 }
-function ObeterEndereço() {}
+function ObterTelefone(idUsuario, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      Telefone: "99932-2427",
+      ddd: "21",
+    });
+  }),
+    2000;
+}
+function ObterEndereço(idUsuario, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      Rua: "Afonso Mello",
+      Bairro: "Rio Varzea",
+    });
+  });
+  1000;
+}
 
-const usuario = Obterusuario();
-const telefone = Obtertelefone();
-console.log("usuario", usuario);
-console.log("telefone", telefone);
+function resolverUsuario(erro, usuario) {
+  console.log("usuario", usuario);
+}
+
+ObterUsuario(function resolverUsuario(error, usuario) {
+  if (error) {
+    console.error();
+    "Deu ruim aqui em jogador", error;
+    return;
+  }
+  ObterTelefone(usuario.id, function resolverTelefone(error1, Telefone) {
+    if (error1) {
+      console.error("Deu ruim no telefone meu jogador", error1);
+      return;
+    }
+    ObterEndereço(usuario.id, function ResolverEndereço(error2, Endereço) {
+      if (error2) {
+        console.error("Deu ruim no endereço em parceiro", error2);
+        return;
+      }
+      console.log(`
+      usuário: ${usuario.Nome}
+      endereço: ${Endereço.Bairro}, Rua: ${Endereço.Rua}
+      telefone: (${Telefone.ddd}) ${Telefone.Telefone}`);
+    });
+  });
+});
