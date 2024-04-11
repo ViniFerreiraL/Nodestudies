c974e752645ac202958bac13342966232a85d52d
Vinicius Ferreira <ferreiravinilugo@gmail.com>
2024-04-11 17:38:27 -0300

    in porgess in for

--- Events/Events.js
+++ Events/Events.js
@@ -1,21 +1,38 @@
-const EventEmitter = require("events");
-class MeuEmissor extends EventEmitter {}
+// const EventEmitter = require("events");
+// class MeuEmissor extends EventEmitter {}
 
-const meuEmissor = new MeuEmissor();
-const novoEmissor = "Usuário click";
-meuEmissor.on(novoEmissor, function (click) {
-  console.log("Entrou um novo usuário", click);
+// const meuEmissor = new MeuEmissor();
+// const novoEmissor = "Usuário click";
+// meuEmissor.on(novoEmissor, function (click) {
+//   console.log("Entrou um novo usuário", click);
+// });
+
+// meuEmissor.emit(novoEmissor, "Ele clicou na barra de rolagem");
+// meuEmissor.emit(novoEmissor, "Ele clicou no ok");
+
+// // let count = 0;
+// // setInterval(function () {
+// //   meuEmissor.emit(novoEmissor, "Ele clicou no ok" + count++);
+// // }, 1000);
+
+// const stdin = process.openStdin();
+// stdin.addListener("data", function (value) {
+//   console.log(`Você digitou:${value.toString().trim()}`);
+// });
+
+const Event = require("events");
+class Emissor extends Event {}
+
+const Emited = new Emissor();
+const newEmited = "Entrou um novo Usuário";
+
+Emited.on(newEmited, function (click) {
+  console.log(newEmited, "e clicou", click);
 });
-
-meuEmissor.emit(novoEmissor, "Ele clicou na barra de rolagem");
-meuEmissor.emit(novoEmissor, "Ele clicou no ok");
-
-// let count = 0;
-// setInterval(function () {
-//   meuEmissor.emit(novoEmissor, "Ele clicou no ok" + count++);
-// }, 1000);
+Emited.emit(newEmited, "na Barra de rolagem");
+Emited.emit(newEmited, "no OK");
 
 const stdin = process.openStdin();
 stdin.addListener("data", function (value) {
-  console.log(`Você digitou:${value.toString().trim()}`);
+  console.log(`Você digitou: ${value.toString().trim()}`);
 });
