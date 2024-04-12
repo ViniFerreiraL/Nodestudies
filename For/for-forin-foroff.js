8c51e9a59af3257f1608223191274aae06892ba6
Vinicius Ferreira <ferreiravinilugo@gmail.com>
2024-04-12 18:52:24 -0300

    finish the studies for, forIn and ForOff in node

--- For/for-forin-foroff.js
+++ For/for-forin-foroff.js
@@ -10,6 +10,20 @@ async function main() {
       name.push(pessoa.name);
     }
     console.timeEnd("for");
+    //////////////////////////////////////////////////////////
+    console.time("forin");
+    for (i in result.results) {
+      const pessoa = result.results[i];
+      name.push(pessoa.name);
+    }
+    console.timeEnd("forin");
+    ///////////////////////////////////////////////////////////
+
+    console.time("ForOF");
+    for (pessoa of result.results) {
+      name.push(pessoa.name);
+    }
+    console.timeEnd("ForOF");
     console.log("name", name);
   } catch (error) {
     console.error("Deu erro aqui", error);
