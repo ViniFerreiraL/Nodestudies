8935d5d54815a914f80beae87d3977e84494c198
Vinicius Ferreira <ferreiravinilugo@gmail.com>
2024-04-15 17:10:01 -0300

    finish all

--- For/for-forin-foroff.js
+++ For/for-forin-foroff.js
@@ -3,28 +3,20 @@ const service = require("./Services");
 async function main() {
   try {
     const result = await service.obterPessoas("a");
-    const name = [];
-    console.time("for");
-    for (let i = 0; i <= result.results.lenght; i++) {
+    let name = [];
+
+    for (let i = 0; i < result.results.length; i++) {
       const pessoa = result.results[i];
       name.push(pessoa.name);
     }
-    console.timeEnd("for");
-    //////////////////////////////////////////////////////////
-    console.time("forin");
     for (i in result.results) {
       const pessoa = result.results[i];
       name.push(pessoa.name);
     }
-    console.timeEnd("forin");
-    ///////////////////////////////////////////////////////////
-
-    console.time("ForOF");
     for (pessoa of result.results) {
       name.push(pessoa.name);
     }
-    console.timeEnd("ForOF");
-    console.log("name", name);
+    console.log("name:", name);
   } catch (error) {
     console.error("Deu erro aqui", error);
   }
