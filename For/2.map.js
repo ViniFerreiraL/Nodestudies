73e350a375e21c6cabf9c6570eb26d8bb8a5b0b2
Vinicius Ferreira <ferreiravinilugo@gmail.com>
2024-04-17 20:22:42 -0300

    Studies array map

---
+++ For/2.map.js
@@ -0,0 +1,32 @@
+const service = require("./Services");
+Array.prototype.MeuMap = function (callback) {
+  const novoArrayMapeado = [];
+  for (let indicide = 0; indicide <= this.length - 1; indicide++) {
+    const mapeado = callback(this[indicide], indicide);
+    novoArrayMapeado.push(mapeado);
+  }
+  return novoArrayMapeado;
+};
+async function main() {
+  try {
+    //Esse results esta pegando os dados do service
+    const results = await service.obterPessoas("a");
+    // //Aqui crio uma array vazia pra implemntar os nomes dentro dela
+    // const names = [];
+    // results.results.forEach(function (item) {
+    //   //Esse name.push pega cada nome do array list
+    //   names.push(item.name);
+    // });
+    // const names = results.results.map(function (pessoa) {
+    //   return pessoa.name;
+    // });
+    // const names = results.results.map((pessoa) => pessoa.name);
+    const names = results.results.MeuMap(function (pessoa, indicide) {
+      return pessoa.name;
+    });
+    console.log("names:", names);
+  } catch (error) {
+    console.error("error", error);
+  }
+}
+main();
--- For/Services.js
+++ For/Services.js
@@ -7,13 +7,13 @@ async function obterPessoas(nome) {
   return response.data;
 }
 
-obterPessoas("r2")
-  .then(function (resultado) {
-    console.log("Resultado", resultado);
-  })
-  .catch(function (error) {
-    console.error("ERROR", error);
-  });
+// obterPessoas("r2")
+//   .then(function (resultado) {
+//     console.log("Resultado", resultado);
+//   })
+//   .catch(function (error) {
+//     console.error("ERROR", error);
+//   });
 module.exports = {
   obterPessoas,
 };
